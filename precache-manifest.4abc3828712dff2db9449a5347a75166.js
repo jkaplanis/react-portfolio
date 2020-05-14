5f885b56535cdbcf6e0ee6050f48b2cf75372d13
jkaplanis <johntkaplanis@gmail.com>
2020-05-13 20:08:10 -0700

    Updates

---
+++ precache-manifest.4abc3828712dff2db9449a5347a75166.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c7f3d9e471822494e30d10705b062b8",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "a95a8571ed1e2cc253d7",
+    "url": "/react-portfolio/static/js/2.ad7f0048.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-portfolio/static/js/2.ad7f0048.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db13b520870bdba06348",
+    "url": "/react-portfolio/static/js/main.f59709fb.chunk.js"
+  },
+  {
+    "revision": "37db3f26b7c0ea36aed0",
+    "url": "/react-portfolio/static/js/runtime-main.d08fb9a1.js"
+  }
+]);
