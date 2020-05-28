9eccceca85edce1852637741b7e77425d926e437
jkaplanis <johntkaplanis@gmail.com>
2020-05-27 18:35:37 -0700

    Updates

---
+++ precache-manifest.c94e0909dce2707b72b14b133a420413.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf656b0b484da1f3d45d0e5569fa8775",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b767013cf1a5edb0a5d4",
+    "url": "./static/js/2.e76959b7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.e76959b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81b17331d2675b456d2a",
+    "url": "./static/js/main.0d1161c9.chunk.js"
+  },
+  {
+    "revision": "766cf3c57d8b7cdb1f1f",
+    "url": "./static/js/runtime-main.c692c199.js"
+  },
+  {
+    "revision": "ddb8054f9c46a7883cf00cdb8a4ec5d7",
+    "url": "./static/media/john-kaplanis-resume.ddb8054f.pdf"
+  }
+]);
